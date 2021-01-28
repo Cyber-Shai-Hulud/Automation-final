881f21d7d5ce45cf3e5653f6c3130bb0839260cb
Bohdan <bod.upkill@gmail.com>
2021-01-28 16:13:51 +0200

    add basket promo check

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,12 +12,12 @@ class PageObject(BasePage):
     def product_name_in_basket_the_same_as_on_page(self):
         added_to_basket_name_product = self.browser.find_element(*ProductPageLocators.ALERT_BOOK_NAME).text
         product_name_on_page = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
-        assert added_to_basket_name_product in product_name_on_page, 'Wrong name displayed in basket'
+        assert added_to_basket_name_product == product_name_on_page, 'Wrong name displayed in basket'
 
     def product_price_same_as_in_basket(self):
         added_to_basket_product_price = self.browser.find_element(*ProductPageLocators.ALERT_BASKET_PRICE).text
         book_price_product_page = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
-        assert added_to_basket_product_price in book_price_product_page, 'Wrong price displayed in basket'
+        assert added_to_basket_product_price == book_price_product_page, 'Wrong price displayed in basket'
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
